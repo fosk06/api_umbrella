61d18d177515702837bba21f7c71bf213695b2df
fosk06 <ttr.fosk@gmail.com>
2019-03-09 20:42:15 +0100

    ok

---
+++ release_old.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# each release is bound to one image tag of the project 
+# first of all , update the image->tag version in the ./helm-chart/values.yaml file
+# get values from the Yaml values.yml file
+TAG=$(yaml get ./helm-chart/values.yaml image.tag)
+SERVICENAME=$(yaml get ./helm-chart/values.yaml fullnameOverride)
+REPOSITORY=$(yaml get ./helm-chart/values.yaml image.repository)
+DOCKERIMAGE="$REPOSITORY:$TAG"
+GITTAG="$SERVICENAME-$TAG"
+
+## the kubernetes namespace, set "production" for production env, or "staging" for staging
+NAMESPACE=production
+
+## the release name
+RELEASENAME="$SERVICENAME-$NAMESPACE"
+
+## login to azure docker registry, you can user the docker login command instead
+# docker login registry -p <password> -u user
+
+## build the image, commit and push to gihub with a tag
+npm run build:prod
+git add .
+git commit -am "release $GITTAG"
+git tag -a $GITTAG -m "release $GITTAG"
+git push origin master
+docker build --rm -f Dockerfile -t $DOCKERIMAGE .
+docker push $DOCKERIMAGE
+
+## upgrade the release, if the relase has already been intalled
+helm upgrade  --recreate-pods $RELEASENAME ./helm-chart
+
+## install the release
+# helm install  --name $RELEASENAME  ./helm-chart --namespace $NAMESPACE
+
+## delete the release
+# helm del --purge $RELEASENAME
+
