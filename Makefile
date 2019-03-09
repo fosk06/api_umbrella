325f719501bea89cd42734d09741db468b3cd41b
fosk06 <ttr.fosk@gmail.com>
2019-03-09 13:31:13 +0100

    release api_umbrella:1.0.1-c7969e1

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 APP_NAME ?= `grep 'app:' mix.exs | sed -e 's/\[//g' -e 's/ //g' -e 's/app://' -e 's/[:,]//g'`
 APP_VSN ?= `grep 'version:' mix.exs | cut -d '"' -f2`
 BUILD ?= `git rev-parse --short HEAD`
+DOCKER_TAG = $(APP_NAME):$(APP_VSN)-$(BUILD)
 
 help:
 	@echo "$(APP_NAME):$(APP_VSN)-$(BUILD)"
@@ -14,13 +15,18 @@ build: ## Build the Docker image
 		-t $(APP_NAME):$(APP_VSN)-$(BUILD) \
 		-t $(APP_NAME):latest .
 
-run: ## Run the app in Docker
+run: build ## run and build image
 	docker run --env-file config/docker.env \
 		--expose 4000 -p 4000:4000 \
 		--rm -it $(APP_NAME):latest
 		
 run_stack: ## Run the stack
 	docker-compose up -d
-	
+
 stop_stack: ## Stop the stack
 	docker-compose stop
+release: 
+	git add .
+	git commit -am "release $(DOCKER_TAG)"
+	git tag -a $(DOCKER_TAG) -m "release $(DOCKER_TAG)"
+	git push origin master
--- release.sh
+++ release.sh
@@ -1,18 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
-# set port
-export PORT=4000
-# get deps
-mix deps.get --only prod
+# each release is bound to one image tag of the project 
+# first of all , update the image->tag version in the ./helm-chart/values.yaml file
+# get values from the Yaml values.yml file
+TAG=$(yaml get ./helm-chart/values.yaml image.tag)
+SERVICENAME=$(yaml get ./helm-chart/values.yaml fullnameOverride)
+REPOSITORY=$(yaml get ./helm-chart/values.yaml image.repository)
+DOCKERIMAGE="$REPOSITORY:$TAG"
+GITTAG="$SERVICENAME-$TAG"
 
-# compile code
-MIX_ENV=prod mix compile
+## the kubernetes namespace, set "production" for production env, or "staging" for staging
+NAMESPACE=production
 
-#build assets with webpack
-mix phx.digest
+## the release name
+RELEASENAME="$SERVICENAME-$NAMESPACE"
 
-# create release 
-MIX_ENV=prod mix release
+## login to azure docker registry, you can user the docker login command instead
+az acr login --name geosentinel
+# docker login geosentinel.azurecr.io -p <password> -u geosentinel
 
-# run the release
-# PORT=4000 _build/prod/rel/api_umbrella/bin/api_umbrella foreground
+## build the image, commit and push to gihub with a tag
+git add .
+git commit -am "release $GITTAG"
+git tag -a $GITTAG -m "release $GITTAG"
+git push origin master
+docker build --rm -f Dockerfile-phpapp -t $DOCKERIMAGE .
+docker push $DOCKERIMAGE
+
+## upgrade the release, if the relase has already been intalled
+helm upgrade  --recreate-pods $RELEASENAME ./helm-chart
+
+## install the release
+# helm install  --name $RELEASENAME  ./helm-chart --namespace $NAMESPACE
+
+## delete the release
+# helm del --purge $RELEASENAME    
